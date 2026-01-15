0b402053e78d6cab25ff0895863307a202bbfd97
Marco <matehuala0484@gmail.com>
2026-01-15 17:07:41 -0600

    Se agregan skils y boton de descarga de cv

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const btn = document.getElementById("btnProyectos");
-    const modal = document.getElementById("ventanaProyectos");
-    const span = document.querySelector(".cerrar");
   
-    btn.onclick = function () {
-      modal.style.display = "block";
-    };
-  
-    span.onclick = function () {
-      modal.style.display = "none";
-    };
-  
-    window.onclick = function (event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    };
   });
   
